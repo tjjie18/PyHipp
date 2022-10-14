2bec9e2b73b11d46fdf72fb0e776b9c5e24b5441
AWS ParallelCluster user <ec2-user@ip-10-0-5-240.ap-southeast-1.compute.internal>
2022-10-14 11:40:48 +0000

    hi

--- ec2snapshot.sh
+++ ec2snapshot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh -o StrictHostKeyChecking=no -i /data/MyKeyPair.pem ec2-user@54.255.227.67 "source ~/.bash_profile; pcluster update-compute-fleet --status STOP_REQUESTED --region ap-southeast-1 --cluster-name MyCluster02; ~/update_snapshot.sh data 2 MyCluster02"
+ssh -o StrictHostKeyChecking=no -i /data/MyKeyPair.pem ec2-user@54.255.227.67 "source ~/.bash_profile; pcluster update-compute-fleet --status STOP_REQUESTED --region ap-southeast-1 --cluster-name MyCluster01; ~/update_snapshot.sh data 2 MyCluster01"
