5b623f6b47f9e31a6bbe5362deb6de7e76f65a1b
AWS ParallelCluster user <ec2-user@ip-10-0-7-78.ap-southeast-1.compute.internal>
2022-10-27 13:17:12 +0000

    update

--- freq-slurm.sh
+++ freq-slurm.sh
@@ -6,11 +6,11 @@
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --cpus-per-task=1	# number of processors per task
-#SBATCH -J "rpllfp"   # job name
+#SBATCH -J "freq"   # job name
 
 ## /SBATCH -p general # partition (queue)
-#SBATCH -o rpllfp-slurm.%N.%j.out # STDOUT
-#SBATCH -e rpllfp-slurm.%N.%j.err # STDERR
+#SBATCH -o freq-slurm.%N.%j.out # STDOUT
+#SBATCH -e freq-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 python -u -c "import PyHipp as pyh; \
